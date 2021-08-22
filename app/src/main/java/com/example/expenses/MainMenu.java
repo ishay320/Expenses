5530da833a7831bddb1561c519064f9fe6a7910c
ishay320 <ishay320@hotmail.com>
2021-08-22 23:12:04 +0300

    main menu

---
+++ app/src/main/java/com/example/expenses/MainMenu.java
@@ -0,0 +1,38 @@
+package com.example.expenses;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity {
+    Button button_fuel_calculation,button_manual_input,button_camera_input;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        button_fuel_calculation = findViewById(R.id.fuel_calculation);
+        button_manual_input = findViewById(R.id.manual_input);
+        button_camera_input = findViewById(R.id.camera_input);
+        button_fuel_calculation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start_activity(MainActivity.class);
+            }
+        });
+        button_manual_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start_activity(ManualInput.class);
+            }
+        });
+    }
+
+
+    void start_activity(Class<?> next_activity){
+        Intent intent = new Intent(this,next_activity);
+        startActivity(intent);
+    }
+}
